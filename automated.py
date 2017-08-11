a9a8f79f6311a2db032629b1ad43ae757624b113
Christopher Vu Le <chrisvle@berkeley.edu>
2017-08-11 14:22:21 -0400

    renaming and fixing load comms

--- automated.py
+++ automated.py
@@ -6,13 +6,13 @@ import pymongo as pg
 from pymongo import MongoClient
 import requests
 
-url = "https://cahl.berkeley.edu:1336/api/email"
+url = "https://cahl.berkeley.edu:1337/api/email"
 headers = {'Content-Type': 'application/json', 'Accept':'application/json'}
 
 # Adding csv to mongo
 csvfile = open('test_pred.csv', 'r')
 reader = csv.DictReader(csvfile)
-mongo_client = MongoClient(host="localhost:1303", port=1303)
+mongo_client = MongoClient(host="localhost:1304", port=1304)
 db_pred = mongo_client.predictions
 db_pred.pred.drop()
 header= ['anon_user_id', 'attrition_prediction', 'completion_prediction', 'certification_prediction']
@@ -31,6 +31,7 @@ db_policy = mongo_client.policies
 cursor = db_policy.policies.find({"intervention": "true", "auto": "true"})
 
 for p in cursor:
+    print(p['name'])
     old_ids = p['ids']
 
     attr_low = float(p['attr'][0])
@@ -39,8 +40,16 @@ for p in cursor:
     comp_high = float(p['comp'][1])
     cert_low = float(p['cert'][0])
     cert_high = float(p['cert'][1])
+    print(attr_low, attr_high, comp_low, comp_high, cert_low, cert_high)
 
-    new_ids = db_pred.pred.find({"$and": [{"attrition_prediction": { "$gte": attr_low }}, {"attrition_prediction": { "$lte": attr_high }}, {"completion_prediction": { "$gte": comp_low }}, {"completion_prediction": { "$lte": comp_high }}, {"certification_prediction": { "$gte": cert_low }}, {"certification_prediction": { "$lte": cert_high }}]})
+    new_ids = db_pred.pred.find(
+    {"$and":
+        [
+            {"$and": [{"attrition_prediction": { "$gte": attr_low }}, {"attrition_prediction": { "$lte": attr_high }}]},
+            {"$and": [{"completion_prediction": { "$gte": comp_low }}, {"completion_prediction": { "$lte": comp_high }}]},
+            {"$and": [{"certification_prediction": { "$gte": cert_low }}, {"certification_prediction": { "$lte": cert_high }}]}
+        ]
+    })
 
     for i in new_ids:
         if i['anon_user_id'] not in old_ids:
@@ -49,3 +58,5 @@ for p in cursor:
             db_policy.policies.find_one_and_update({"_id": p['_id']}, {"$set": {"ids":updated}})
             data_to_send = {"pass": "sadfvkn88asVLS891", "ids": [i['anon_user_id']], "body": p['body'], "subject": p['subject'], "reply": p['reply']}
             r = requests.post(url, data=json.dumps(data_to_send), headers=headers)
+
+    break
--- server.js
+++ server.js
@@ -5,18 +5,60 @@ var https = require('https');
 var http = require('http');
 var fs = require('fs');
 var auth = require('basic-auth');
-var csv = require('csvtojson');
 var nodemailer = require('nodemailer');
+var $ = jQuery = require('jquery');
+require(process.cwd() + '/jquery.csv.min.js');
+
+var Policy     = require(process.cwd() + '/app/models/policy');
+
+var mongoose   = require('mongoose');
+mongoose.connect('mongodb://cahl.berkeley.edu:1304/policies'); // ******put a /events after port to save into that db
 
 
-var csvFilePath = 'studentEmails.csv';  // change this to location of email csv
-var csvPredictionsPath = process.cwd() + '/studentPredictions.csv'; // change this to location of prediction csv
-var gmailUsername = 'me%40gmail.com'; // change to the account you want to have sending emails, escape @ as '%40'
-var gmailPassword = 'veryLongPassword';  //change to match
+// NEED TO CHANGE MASTER to get actual, copy over
+// var csvWeekly = '../MASTER_user_info.csv';  // change this to location of email csv
+var csvWeekly = 'test.csv';  // change this to location of email csv
+// var csvDaily = process.cwd() + '../studentPredictions.csv'; // change this to location of prediction csv
+// var csvDaily = '/deepedu/research/moocdrop/live-data/new_prediction.csv';
+var csvDaily = '/deepedu/research/moocdrop/live-data/moocdrop/test_pred.csv';
+console.log(process.cwd());
+var gmailUsername = 'berkeleyx.communications%40gmail.com'; // change to the account you want to have sending emails, escape @ as '%40'
+var gmailPassword = 'AGfsdj45j&2jkfasdbjk$309vshadjkfhsadschsd32jhkjh!';
+//change to match
 var secretUsername = 'john';  // change: must be shared with client.html
 var secretPassword = 'secret'; // change: must be shared with client.html
 
-var transporter = nodemailer.createTransport('smtps://' + gmailUsername + ':' + gmailPassword + '@smtp.gmail.com');
+// making anonID to email dict
+var index_csv = csvWeekly;
+var anon_to_email = {};
+var all_ids = [];
+fs.readFile(index_csv, 'UTF-8', function(err, csv) {
+  $.csv.toArrays(csv, {}, function(err, data) {
+    for(var i=1, len=data.length; i<len; i++) {
+      if (data[i][3]) {
+          all_ids.push(data[i][1]);
+          anon_to_email[data[i][1]] = {'email': data[i][3]}; // email
+          if (data[i][4]) {
+            anon_to_email[data[i][1]]['first'] = data[i][4]; // first name
+          }
+          if (data[i][5]) {
+            anon_to_email[data[i][1]]['last'] = data[i][5]; // last name
+          }
+      }
+      else {
+        continue;
+      }
+    }
+  });
+});
+
+// making policy dictionary
+// var policy_dict = {};
+// policy_dict['test'] = {'ids': ['12','54'], 'subject' : 'hello world', 'body': 'this is a test', 'name': '07/23/17 hello world', 'comp' : [15,22], 'attr' : [50,60], 'cert' : [78, 90], 'auto' : true};
+// policy_dict['test2'] = {'ids': ['82','54'], 'subject' : 'hello world88', 'body': '888this is a te8st', 'name': '07/23/17 h8e8l8l8o world', 'comp' : [5,82], 'attr' : [50,80], 'cert' : [78, 80], 'auto' : false};
+
+
+var transporter = nodemailer.createTransport('smtps://' + gmailUsername + ':' + encodeURI(gmailPassword) + '@smtp.gmail.com');
 
 // These should exist on your server to use https
 var pkey = fs.readFileSync('/etc/ssl/cahl.key').toString();
@@ -34,7 +76,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // set our port
-var port = process.env.PORT || 1335;
+var port = process.env.PORT || 1337;
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -60,43 +102,63 @@ function checkCredentials(credentials) {
     return true;
   }
 }
+
  // on routes that end in /email
  // ----------------------------------------------------
 router.route('/email')
     // get the ids for emails
     .post(function(req, res) {
-        var fullList = [];
-        var emailLookup = {};
-        csv()
-        .fromFile(csvFilePath)
-        .on('json', function(jsonObj) {
-            // combine csv header row and csv line to a json object
-            // jsonObj.a ==> 1 or 4
-            fullList.push(jsonObj);
-        }).on('done', function(error) {
-            for (var i = 0; i < fullList.length; i++) {
-                var item = fullList[i];
-                emailLookup[item.anonymizedId] = item.email;
-            }
-            var students = req.body.students;
-            var email = req.body.email;
-            for (var i = 0; i < students.length; i++) {
-                var student = students[i];
-                sendEmail(emailLookup[student.anonymizedId], email.Subject, email.Content, function (err) {
-                    if (err) {
-                        console.log("email send failed");
-                    }
+      if (req.body.pass === 'sadfvkn88asVLS891') {
+        var ids = req.body.ids;
+        if (req.body.ann === 'true') {
+          ids = all_ids;
+        }
+        for (var j = 0; j < ids.length; j++) {
+          var id = ids[j];
+          if (id in anon_to_email) {
+              var message_body = req.body.body;
+              console.log(anon_to_email);
+              console.log(anon_to_email[id]['first'], anon_to_email[id]['last']);
+              if (anon_to_email[id]['first']) {
+                message_body = message_body.replace('[:firstname:]', anon_to_email[id]['first']);
+              }
+              else {
+                message_body = message_body.replace('[:firstname:]', '');
+              }
+              if (message_body.indexOf('[:fullname:]') !== -1) {
+                message_body = message_body.replace('[:fullname:]', anon_to_email[id]['first'] + " " + anon_to_email[id]['last']);
+              }
+              else {
+                message_body = message_body.replace('[:fullname:]', '');
+              }
+              console.log(message_body);
+              sendEmail(anon_to_email[id].email, req.body.subject, message_body, req.body.reply, function (err) {
+                  if (err) {
+                      console.log(err);
+                      console.log("email send failed");
+                  }
                 });
-            }
-            res.send('sent');
-        });
+          }
+          else {
+            continue;
+          }
+        }
+
+        res.send('sent');
+      }
+      else {
+        res.send('Access Denied');
+        res.end('Access denied');
+      }
     });
-function sendEmail(email, subject, content, cb) {
+
+function sendEmail(email, subject, content, reply, cb) {
     var mailOptions = {
         from: gmailUsername, // sender address
         to: email, // list of receivers
         subject: subject, // Subject line
-        text: content // plaintext body
+        text: content, // plaintext body
+        replyTo: reply
     };
 
     // send mail with defined transport object
@@ -107,6 +169,7 @@ function sendEmail(email, subject, content, cb) {
         cb(null);
     });
 }
+
 router.route('/predictions').get(function(req, res) {
   var credentials = auth(req);
   if (!checkCredentials(credentials)) {
@@ -115,7 +178,70 @@ router.route('/predictions').get(function(req, res) {
     res.end('Access denied');
     return;
   }
-  res.sendFile(csvPredictionsPath);
+  res.sendFile(csvDaily);
+});
+
+router.route('/interventions').get(function(req, res) {
+  query = Policy.find({"intervention": "true" }).sort("timestamp");
+
+  query.exec(function (err, output) {
+      if (err) {
+        return next(err);
+      }
+      else {
+        res.json(output);
+      }
+    });
+});
+
+router.route('/announcements').get(function(req, res) {
+  query = Policy.find({"intervention": "false" }).sort("timestamp");
+
+  query.exec(function (err, output) {
+      if (err) {
+        return next(err);
+      }
+      else {
+        res.json(output);
+      }
+    });
+});
+
+router.route('/save').post(function(req, res) {
+  var policy = new Policy();
+  if (req.body.intervention === 'true') {
+    policy.ids = req.body.ids;
+    policy.comp = req.body.comp;
+    policy.attr = req.body.attr;
+    policy.cert = req.body.cert;
+    policy.auto = req.body.auto;
+  }
+  else {
+    policy.ids = ''
+    policy.comp = [];
+    policy.attr = [];
+    policy.cert = [];
+    policy.auto = 'false'
+  }
+  policy.name = req.body.name;
+  policy.subject = req.body.subject;
+  policy.body = req.body.body;
+  policy.reply = req.body.reply;
+  policy.timestamp = req.body.timestamp;
+  policy.intervention = req.body.intervention;
+
+  policy.save(function(err) {
+    if (err) {
+      console.log("error when saving policy");
+      res.end();
+      return;
+    }
+  });
+});
+
+router.route('/stop').post(function(req, res) {
+  Policy.update({"name": req.body.name}, {"$set": {"auto": "false"}}).exec();
+  res.json({ message: 'Successfully stopped' });
 });
 
 // This route exists only for testing your password
